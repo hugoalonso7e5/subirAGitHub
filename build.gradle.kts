c070b549a9e7c85e53a371b6efcb9f4416dc78d3
hugo [itb] alonso velasco <hugo.alonso.7e5@itb.cat>
2024-01-22 22:31:33 +0100

    Fitxer de test creat.

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,7 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
     dokkaHtmlPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.9.10")
 }
 
---
+++ src/test/kotlin/MainKtTest.kt
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.Assertions.*
+import kotlin.test.Test
+
+class MainKtTest {
+
+    @Test
+    fun sumaMatrizInt() {
+
+    }
+
+    @Test
+    fun calcularMitjana() {
+    }
+
+    @Test
+    fun quants5HiHa() {
+    }
+
+    @Test
+    fun mitajanaDe5() {
+    }
+
+    @Test
+    fun sumarMajor5() {
+    }
+
+    @Test
+    fun testImprimirMatriz() {
+    }
+
+    @Test
+    fun retornarChar() {
+    }
+
+    @Test
+    fun modificarPer_() {
+    }
+}
